d3a3fd9da5ce4cf8e0b8ab2d4ac91319055379e1
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2024-08-21 16:06:46 +0400

    v2

--- core/main/urls.py
+++ core/main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', views.login_request, name='login'),
+    path('register/',views.register_request, name='register'),
+    path('logout/', views.logout_request, name='logout'),
+    path('userpage/', views.userpage, name='userpage'),
 ]
--- core/main/views.py
+++ core/main/views.py
@@ -1,9 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Product
+from .forms import NewUserForm, ProductForm
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 def index(request):
     category_list = Category.objects.all()
     return render(request, 'index.html', context={
         'category_list':category_list
+    })
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("index")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render(request=request, template_name="register.html", context={"register_form":form})
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("index")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="login.html", context={"login_form":form})
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("index")
+
+
+def userpage(request):
+    category_list = Category.objects.all()
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('userpage')  # Redirect to the same page after adding the product
+    else:
+        form = ProductForm()
+
+    return render(request, 'userpage.html', context={
+        'category_list': category_list,
+        'form': form,
     })
